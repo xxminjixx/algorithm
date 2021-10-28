bd1159eaf373187503e0399affdeb8ec221a1e89
Minji Choi <so8376@gmail.com>
2021-10-29 02:53:43 +0900

    1. 1부터 N까지 M 배수합
    ex) 15 3 입력
    => 1부터 15(N) 사이에 있는 3(M)의 배수의 합 == 45

---
+++ study/1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, m, i, sum = 0;
+    std::cin >> n >> m;
+
+    for (i = 1; i <= n; i++)
+    {
+        if (i % m == 0)
+        {
+            sum = sum + i;
+        }
+    }
+
+    cout << sum;
+    return 0;
+}
+
